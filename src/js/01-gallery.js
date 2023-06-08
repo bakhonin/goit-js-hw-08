81b87a08b9131fc1d87a4d87bb9b1c9527c5decc
Denys <bakhonin1@gmail.com>
2023-06-08 16:21:26 +0200

    home-work 8 (task 1)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+
+const createGalleryItems = galleryItems.map(({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+  </a>
+</li>`
+);
+
+galleryList.insertAdjacentHTML('beforeend', createGalleryItems.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
