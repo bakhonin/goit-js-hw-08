b674b06dd4e9704e75e42f2f6ced2a51875e5146
Denys <bakhonin1@gmail.com>
2023-06-08 18:29:40 +0200

    home-work 8 (task 2)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import throttle from "lodash.throttle";
+import Player from "@vimeo/player"
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(function (data){
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+}),1000);
+
+
+player.on('timeupdate',throttle(function(data){
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(data.seconds));
+}),1000)
+const getTime = localStorage.getItem(STORAGE_KEY); 
+  if(getTime){
+   player.setCurrentTime(JSON.parse(getTime));
+  }; 
+
+player.setCurrentTime()
+.then(function(seconds) {})
+.catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+            default:
+            break;
+    }
+});
